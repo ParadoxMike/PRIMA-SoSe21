790f2dfe712fa3dc0b335aec32f81dd03d4bd07c
Mike <mike.blank@lantia.de>
2021-07-21 13:37:50 +0200

    DoodleSpace: remove dummy

--- DoodleSpace/functions.ts
+++ DoodleSpace/functions.ts
@@ -1,15 +1,5 @@
 namespace DoodleSpace {
     import ƒ = FudgeCore;
 
-    export function generateDummy(): ƒ.Node {
-        let dummy: ƒ.Node = new ƒ.Node("Dummy");
-        dummy.addComponent(new ƒ.ComponentTransform());
-        dummy.mtxLocal.translateZ(-1);
-        dummy.addComponent(new ƒ.ComponentMesh(new ƒ.MeshQuad("dummyMesh")));
-        let dummyMaterial: ƒ.Material = new ƒ.Material("Dummy Material", ƒ.ShaderUniColor, new ƒ.CoatColored(new ƒ.Color(0, 1, 0, 1)));
-        let cmpMaterial: ƒ.ComponentMaterial = new ƒ.ComponentMaterial(dummyMaterial);
-        dummy.addComponent(cmpMaterial);
-
-        return dummy;
-    }
+    
 }
